d15aa2f365806b951846257416fd592226adecda
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-07-02 16:47:12 +1000

    added chat program

---
+++ s6/chat/client.py
@@ -0,0 +1,79 @@
+import os
+import socket
+from threading import Thread
+
+
+class Client:
+
+    # инициализация объекта
+    def __init__(self, server: (str, int), nickname: str):
+        try:
+            self.__nickname = nickname
+            # Connecting To Server
+            self.__client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.__client.connect(server)
+            self.__receive_thread = Thread(target=self.__receive)
+            self.__write_thread = Thread(target=self.__write)
+        # обработка ошибки подключения к серверу
+        except ConnectionRefusedError:
+            print('Сервер выключен')
+            exit()
+
+    # Starting Threads For Listening And Writing
+    def start(self):
+        self.__receive_thread.start()
+        self.__write_thread.start()
+
+    # Listening to Server and Sending Nickname
+    def __receive(self):
+        while True:
+            try:
+                # Receive Message From Server
+                # If 'NICK' Send Nickname
+                message = self.__client.recv(1024).decode('utf-8')
+                if message == 'NICK':
+                    self.__client.send(self.__nickname.encode('utf-8'))
+                else:
+                    print(message)
+            except ConnectionResetError:
+                # Выход из программы при отсутствии подключения к серверу
+                print("Сервер выключен")
+                self.exit()
+
+    # Ввод сообщений и отправка их на сервер
+    def __write(self):
+        while True:
+            try:
+                string = input()
+                if string != '':
+                    message = '{}: {}'.format(self.__nickname, string)
+                    self.__client.send(message.encode('utf-8'))
+            # Обработка ошибок и выход из программы
+            except:
+                self.exit()
+
+    def exit(self):
+        self.__client.close()
+        print('Выход из программы')
+        os._exit(0)
+
+
+def exit():
+    print('Выход из программы')
+    os._exit(0)
+
+
+def main():
+    nickname = ''
+    try:
+        nickname = input("Choose your nickname: ")
+    # Обработка ошибки и выход из программы
+    except EOFError:
+        exit()
+    # Создание объекта с передачей IP и порта сервера и имени пользователя
+    client = Client(('127.0.0.1', 55555), nickname)
+    client.start()
+
+
+if __name__ == "__main__":
+    main()
---
+++ s6/chat/server.py
@@ -0,0 +1,77 @@
+import os
+import socket
+import threading
+import keyboard
+
+# Connection Data
+host = ('127.0.0.1', 55555)
+
+# Starting Server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(host)
+server.listen()
+
+# Lists For Clients and Their Nicknames
+clients = dict()
+
+
+# Sending Messages To All Connected Clients
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+# Handling Messages From Clients
+def handle(client):
+    while True:
+        try:
+            # Broadcasting Messages
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            # Removing And Closing Clients
+            nickname = clients.get(client)
+            clients.pop(client)
+            broadcast('{} left!'.format(nickname).encode('utf-8'))
+            print(f'{nickname} disconnected')
+            client.close()
+            break
+
+
+# Receiving / Listening Function
+def receive():
+    while True:
+        # if keyboard.read_key() == "q":
+        #     break
+        # Accept Connection
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+
+        # Request And Store Nickname
+        client.send('NICK'.encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
+        clients[client] = nickname
+
+        # Print And Broadcast Nickname
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('utf-8'))
+        client.send('Connected to server!'.encode('utf-8'))
+
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+# выход из программы
+def stop():
+    os._exit(0)
+
+
+print("Server if listening...")
+# выход из программы по хоткею
+keyboard.add_hotkey('ctrl+s', stop)
+try:
+    receive()
+# обработка ошибки чтобы не выключался сервер
+except ConnectionResetError:
+    pass
